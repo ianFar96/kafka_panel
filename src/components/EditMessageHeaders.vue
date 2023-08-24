cc70c04b547b13d0da8a5f3a8cb0eccb2f557b96
Ian <ian.farre@mia-platform.eu>
2023-08-24 19:56:12 +0200

    feat: UI for editing headers

--- src/components/EditMessageHeaders.vue
+++ src/components/EditMessageHeaders.vue
@@ -1,8 +1,7 @@
 
 <script setup lang="ts">
-import { appWindow } from '@tauri-apps/api/window';
 import MonacoEditor from 'monaco-editor-vue3';
-import { nextTick, onBeforeUnmount, onMounted, ref } from 'vue';
+import { ref } from 'vue';
 import { messageToSendMessage } from '../services/utils';
 import { ParsedHeaders } from '../types/message';
 
@@ -17,11 +16,8 @@ const sendMessage = props.headers && messageToSendMessage({
 	value: {},
 	headers: props.headers
 });
-const headers = ref(sendMessage?.headers ?? null);
-
-onMounted(() => {
-	setMonacoEditorSizes();
-});
+const headersToEdit = sendMessage?.headers ? Object.entries(sendMessage?.headers).map(([key, value]) => ({key, value})) : null;
+const headers = ref(headersToEdit);
 
 const editorOptions = {
 	colorDecorators: true,
@@ -41,55 +37,63 @@ const editorOptions = {
 	folding: false
 };
 
+const addHeader = () => {
+	if (headers.value) {
+		headers.value.push({
+			key: '',
+			value: ''
+		});
+	} else {
+		headers.value = [{
+			key: '',
+			value: ''
+		}];
+	}
+};
+
+const removeHeader = (index: number) => {
+	headers.value!.splice(index, 1);
+	if (headers.value!.length <= 0) {
+		headers.value = null;
+	}
+};
+
 const handleSubmit = async () => {
-	await props.submit(headers.value);
+	const headersToSubmit = headers.value ? headers.value.reduce((acc, header) => ({
+		...acc,
+		[header.key]: header.value
+	}), {}) : null;
+
+	await props.submit(headersToSubmit);
 
 	// Reset form
-	headers.value = {};
+	headers.value = [];
 };
-
-type Sizes = {
-  width: number
-  height: number
-}
-let monacoEditorSizes = ref<Sizes | undefined>();
-
-// We only need one of the slots since they are the same
-const valueSlotRef = ref<HTMLElement | null>(null);
-
-const setMonacoEditorSizes = () => {
-	monacoEditorSizes.value = undefined;
-
-	nextTick(() => {
-		monacoEditorSizes.value = {
-			height: (valueSlotRef.value?.clientHeight || 250),
-			width: (valueSlotRef.value?.clientWidth || 500),
-		};
-	});
-};
-
-const unlisten = await appWindow.onResized(() => {
-	setMonacoEditorSizes();
-});
-
-onBeforeUnmount(() => {
-	unlisten();
-});
-
 </script>
 
 <template>
-	<form class="h-full w-full" @submit="handleSubmit()">
-		<template v-if="headers">
-			<div class="mb-4" v-for="key in Object.keys(headers)" :key="key">
-				<label class="block mb-2">{{ key }}</label>
+	<form @submit="handleSubmit()">
+		<div class="" v-if="headers">
+			<div ref="headerItemRef" class="mb-4" v-for="header, index in headers" :key="header.key">
+				<div class="flex items-end mb-2">
+					<input type="text" v-model.lazy="header.key"
+						class="block bg-transparent outline-none border-b border-gray-400 py-1 w-full" placeholder="Name*">
+					<i class="bi-trash hover:text-red-500 transition-colors cursor-pointer text-lg border-b border-gray-400"
+						@click="removeHeader(index)"></i>
+				</div>
+
+				<!-- FIXME: writing first the name of the headers breaks it's value monaco editor -->
 				<div class="h-full rounded-xl overflow-hidden">
-					<MonacoEditor v-if="monacoEditorSizes" theme="vs-dark" :options="editorOptions" language="json"
-						:width="monacoEditorSizes.width" :height="monacoEditorSizes.height" v-model:value="headers[key]">
+					<MonacoEditor theme="vs-dark" :options="editorOptions" language="json"
+						:width="750" :height="250" v-model:value="header.value">
 					</MonacoEditor>
 				</div>
 			</div>
-		</template>
+		</div>
+		<button class="border border-white rounded py-1 px-4 hover:border-orange-400 transition-colors hover:text-orange-400 w-full"
+			type="button" @click="addHeader">
+			Add Header
+		</button>
 		<div class="mt-8 flex justify-end flex-none">
 			<button type="submit"
 				class="border border-white rounded py-1 px-4 hover:border-green-500 transition-colors hover:text-green-500">
